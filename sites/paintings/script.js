b49062a0a380980569469a71e26f63089cc62566
sergkon4 <66921687+sergkon4@users.noreply.github.com>
2023-01-05 21:23:23 +0200

    Update script.js

--- sites/paintings/script.js
+++ sites/paintings/script.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+$(document).ready(function(){
+   $("#smallmenu").on("click","a", function (event) {
+       event.preventDefault();
+       var id  = $(this).attr('href'),
+           top = $(id).offset().top;
+       $('body,html').animate({scrollTop: top}, 1500);
+   });
+});
